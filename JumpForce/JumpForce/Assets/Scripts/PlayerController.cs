6fe393d3dac8fc188ecfdb5373148abcc7f01b08
Zachary Vasquez <vasquezz@roverkids.org>
2025-05-14 13:22:25 -0400

    i

--- JumpForce/JumpForce/Assets/Scripts/PlayerController.cs
+++ JumpForce/JumpForce/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
